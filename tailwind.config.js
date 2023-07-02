ec43b2407753f50a2a69b19eb37eaeea66cac0a0
Francisco <francisco@fromokaytogreat.com>
2023-07-01 20:31:51 -0700

    Extend Tailwind CSS theme to match Overdue colors
    https://github.com/itisoverdue-com/overdue-website/blob/main/src/styles/_mixin.scss

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,32 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // black to white
+        black: '#000',
+        'darkest-grey': '#111',
+        'darker-grey': '#222',
+        'dark-grey': '#333',
+        grey: '#666',
+        'light-grey': '#999',
+        'lighter-grey': '#ccc',
+        'lightest-grey': '#eee',
+        white: '#fff',
+
+        // brand colors
+        primary: '#fdd92a',
+        primaryDark: '#ffc107',
+        background: '#f6f5f2',
+
+        // status colors
+        error: '#e84118',
+        success: '#009432',
+      },
+      fontFamily: {
+        'sans': ['Roboto', 'sans-serif'], 
+      },
+    },
   },
   plugins: [],
 }
