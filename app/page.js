c771646ea3197851304414502bee38bedc355b37
Francisco <francisco@fromokaytogreat.com>
2023-07-08 11:30:01 -0700

    fix path with typo for Footer

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
    return (
       <main>
          <FullBleedContainer sx="h-96 bg-blue-200">
-            <p className="bg-red-200 h-">Hello World</p>
+            <p className="bg-red-200 h-">Hello World!</p>
          </FullBleedContainer>
       </main>
    )
