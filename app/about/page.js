2b1d5030278b44555efee2e3be2b4b34683cf90d
Tim Kent <timdekent@gmail.com>
2023-07-11 09:55:59 -0700

    update card, updates to about page

--- app/about/page.js
+++ app/about/page.js
@@ -1,7 +1,58 @@
 import Image from "next/image"
 
+import PageHero from "@/components/shared/PageHero"
+import FullBleedContainer from "@/components/Layout/Container/FullBleedContainer"
 import Card from "@/components/shared/Card"
 
 export default function About() {
-   return <article></article>
+   return (
+      <div>
+         <PageHero
+            page="about us"
+            backgroundImageSrc="https://res.cloudinary.com/di7ejl8jx/image/upload/v1688441386/backgrounds/about_avmkds.jpg"
+         />
+         {/* <FullBleedContainer
+            sx="bg-background"
+            childSx="py-28 flex flex-col justify-center items-center text-center md:py-32"
+         > */}
+         <FullBleedContainer sx="bg-background" childSx="py-28 md:py-32">
+            <div className="flex flex-col md:flex-row justify-center items-center md:items-start text-center">
+               {/* Title, Accent Line, Subheader */}
+               <div className="md:w-full">
+                  <h2 className="text-4xl md:text-5xl font-bold">
+                     IT IS OVERDUE
+                  </h2>
+                  <div className="border-t-8 border-primary w-20 rounded-full my-4 md:my-5 md:w-28 mx-auto md:ml-4" />
+                  <h3 className="mx-auto mt-8 text-xl md:text-4xl md:leading-[3rem] font-medium text-left px-8 md:px-6">
+                     OVERDUE is a collective of individuals who pick up trash
+                     together to make their communities cleaner, greener and
+                     friendlier.
+                  </h3>
+               </div>
+               <div className="my-12">
+                  <Card variant="image" imgWidth="w-[85%] lg:w-full">
+                     <Image
+                        alt="text"
+                        src="https://res.cloudinary.com/di7ejl8jx/image/upload/v1688441387/about/intro_ixa5ny.jpg"
+                        width={700}
+                        height={500}
+                     />
+                  </Card>
+                  <Card
+                     variant="text"
+                     sx="w-[95%] top-[-35px] md:top-3/4 md:left-0"
+                  >
+                     <p className="md:text-lg md:leading-10 text-left">
+                        We started in October 2020 in the San Fernando Valley,
+                        CA and have picked up over 58,000 lbs of trash ever
+                        since. Our goal is to develop a network of empowered
+                        leaders of all ages who are taking responsibility in
+                        their neighborhoods.
+                     </p>
+                  </Card>
+               </div>
+            </div>
+         </FullBleedContainer>
+      </div>
+   )
 }
--- components/shared/Card.js
+++ components/shared/Card.js
@@ -1,16 +1,15 @@
 /**
  * Function to generate the className string based on the given props.
  * @param {string} variant - The variant style of the card.
- * @param {string} imgWidth - The width settings for the image across all breakpoints.
- * @param {string} imgAspect - The aspect ratio setting for the image (sets height in relation to width).
+ * @param {string} imgWidth - The width settings for the image container div across all breakpoints.
  * @return {string} - The generated className string.
  */
-function generateClassName(variant, imgWidth, imgAspect) {
+function generateClassName(variant, imgWidth) {
    const baseClasses = "relative mx-auto rounded-3xl"
 
    const variantClasses = {
       text: `w-full max-w-[700px] py-6 px-8 shadow-[0_4px_12px_0_rgba(0,0,0,0.2)] leading-8 bg-white z-[1] text-sm`,
-      image: `overflow-hidden max-w-full ${imgWidth} ${imgAspect}`,
+      image: `overflow-hidden max-w-full ${imgWidth}`,
    }
 
    return `${baseClasses} ${variantClasses[variant]}`
@@ -21,14 +20,8 @@ function generateClassName(variant, imgWidth, imgAspect) {
  * @param {string} sx - Additional CSS rules (will override any existing rules for the same property)
  * @returns
  */
-export default function Card({
-   children,
-   sx = "",
-   variant,
-   imgWidth,
-   imgAspect,
-}) {
-   const className = generateClassName(variant, imgWidth, imgAspect)
+export default function Card({ children, sx = "", variant, imgWidth }) {
+   const className = generateClassName(variant, imgWidth)
 
    return <div className={`${className} ${sx}`}>{children}</div>
 }
@@ -49,12 +42,12 @@ Example Card with "image" variant
   <Card
     variant="image"
     imgWidth="w-[90%] lg:w-[700px]"
-    imgAspect="aspect-[7/5]"
     sx="border-2"
   >
     <Image
         src="..."
-        fill={true} // Causes the image to fill the parent element
+        width={700}
+        height={500}
         alt="Alt text"
     />
   </Card>
