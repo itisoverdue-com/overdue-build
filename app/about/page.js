7e7885212a9502ec317859cc31a735cfafae535c
Tim Kent <timdekent@gmail.com>
2023-07-11 13:59:17 -0700

    update Card, it is overdue section

--- app/about/page.js
+++ app/about/page.js
@@ -11,46 +11,58 @@ export default function About() {
             page="about us"
             backgroundImageSrc="https://res.cloudinary.com/di7ejl8jx/image/upload/v1688441386/backgrounds/about_avmkds.jpg"
          />
-         {/* <FullBleedContainer
-            sx="bg-background"
-            childSx="py-28 flex flex-col justify-center items-center text-center md:py-32"
-         > */}
-         <FullBleedContainer sx="bg-background" childSx="py-28 md:py-32">
-            <div className="flex flex-col md:flex-row justify-center items-center md:items-start text-center">
-               {/* Title, Accent Line, Subheader */}
-               <div className="md:w-full">
-                  <h2 className="text-4xl md:text-5xl font-bold">
-                     IT IS OVERDUE
-                  </h2>
-                  <div className="border-t-8 border-primary w-20 rounded-full my-4 md:my-5 md:w-28 mx-auto md:ml-4" />
-                  <h3 className="mx-auto mt-8 text-xl md:text-4xl md:leading-[3rem] font-medium text-left px-8 md:px-6">
+         <FullBleedContainer
+            sx="bg-background border"
+            childSx="py-28 md:py-32 px-6 lg:px-12"
+         >
+            {/* Section 1 */}
+            <div className="flex flex-col justify-center items-center text-center md:flex-row md:flex-wrap xl:px-12">
+               {/* Subheader */}
+               <div className="md:w-[55%] md:pr-12 xl:pr-4 lg:w-[45%]">
+                  <h2 className="md:text-left">IT IS OVERDUE</h2>
+                  <div className="border-t-8 border-primary w-20 rounded-full my-4 md:my-5 md:w-28 mx-auto md:mx-0" />
+                  <h3 className="mx-auto mt-8 px-4 sm:px-8 md:px-0 sm:text-2xl md:text-3xl md:leading-[3rem] font-medium text-left">
                      OVERDUE is a collective of individuals who pick up trash
                      together to make their communities cleaner, greener and
                      friendlier.
                   </h3>
                </div>
-               <div className="my-12">
-                  <Card variant="image" imgWidth="w-[85%] lg:w-full">
-                     <Image
-                        alt="text"
-                        src="https://res.cloudinary.com/di7ejl8jx/image/upload/v1688441387/about/intro_ixa5ny.jpg"
-                        width={700}
-                        height={500}
-                     />
-                  </Card>
-                  <Card
-                     variant="text"
-                     sx="w-[95%] top-[-35px] md:top-3/4 md:left-0"
-                  >
-                     <p className="md:text-lg md:leading-10 text-left">
-                        We started in October 2020 in the San Fernando Valley,
-                        CA and have picked up over 58,000 lbs of trash ever
-                        since. Our goal is to develop a network of empowered
-                        leaders of all ages who are taking responsibility in
-                        their neighborhoods.
-                     </p>
-                  </Card>
-               </div>
+
+               {/* Image Card */}
+               <Card
+                  variant="image"
+                  sx="w-[75vw] aspect-[7/5] mt-8 md:h-[400px] md:w-[45%] md:mt-0 lg:w-[55%] lg:max-w-[560px] lg:mr-0"
+               >
+                  <Image
+                     alt="text"
+                     src="https://res.cloudinary.com/di7ejl8jx/image/upload/v1688441387/about/intro_ixa5ny.jpg"
+                     fill
+                     style={{
+                        objectFit: "cover",
+                     }}
+                  />
+               </Card>
+
+               {/* Text Card */}
+               <Card
+                  variant="text"
+                  sx="top-[-35px] md:top-0 md:mt-8 lg:top-[-50px] lg:mr-12 min-[1512px]:top-[-100px]"
+               >
+                  <p>
+                     We started in October 2020 in the San Fernando Valley, CA
+                     and have picked up over 58,000 lbs of trash ever since. Our
+                     goal is to develop a network of empowered leaders of all
+                     ages who are taking responsibility in their neighborhoods.
+                  </p>
+               </Card>
+            </div>
+            {/* Section 2 */}
+            <div>
+               {/* How We Got Started */}
+               <div></div>
+
+               {/* Our Philosophy */}
+               <div></div>
             </div>
          </FullBleedContainer>
       </div>
