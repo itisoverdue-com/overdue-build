31290b880305dd21ec473bc79b432f106577a09e
Francisco <francisco@fromokaytogreat.com>
2023-07-12 20:37:03 -0700

    :art: add Hero component for Landing page

--- components/Landing/Hero.js
+++ components/Landing/Hero.js
@@ -2,33 +2,36 @@ import Link from 'next/link';
 import FullBleedContainer from '@/components/Layout/Container/FullBleedContainer';
 import Button from '@/components/shared/Button';
 
+const SubHeading = ({ children }) => (
+    <h2 className="font-normal text-2xl md:text-4xl">{children}</h2>
+);
+
 export default function Hero({ page, backgroundImageSrc = "" }) {
     return (
        <FullBleedContainer
-          sx="h-[80vh] md:h-[50vh]"
+          sx="h-[100vh] md:h-[80vh] lg:h-[70vh] xl:h-[60vh]"
           backgroundImageSrc={`${backgroundImageSrc}`}
-          childSx="flex justify-center items-start h-full relative text-white text-2xl
+          childSx="flex sm:items-start sm:justify-center sm:pl-20 items-center justify-center  flex-col h-full relative text-white text-center text-2xl
           md:text-4xl lg:text-5xl xl:text-6xl font-bold flex-col space-y-5"
        >
-          {/* Header w/ underline */}
           <h1>
              {page}
              <span className="mt-2 block w-1/3 border-t-8 border-primary rounded-2xl mx-auto" />
           </h1>
-        {/* Subheader */}
-        <div className="flex flex-col justify-center items-start">
-            <h2 className="text-2xl md:text-4xl font-normal">
-                We are the future.  We are the solution.
-            </h2>
-            <h2 className="text-2xl md:text-4xl font-normal">
-                We will make your city the cleanest city.
-            </h2>
-        </div>
-        {/* Button */}
-        <div className="flex flex-col justify-center items-center">
-            <Button variant="primary" size="lg">Join Our Team!</Button>
+          
+          <div className="flex flex-col items-start justify-center">
+          <SubHeading>
+                    We are the future.  We are the solution.
+                </SubHeading>
+                <SubHeading>
+                    We will make your city the cleanest city.
+                </SubHeading>
+          </div>
+
+          <div className="flex flex-col sm:flex-row items-center justify-center gap-8">
             <Button variant="outline" size="lg">Join Our Team!</Button>
-        </div>
+            <Button variant="primary" size="lg">Donate Now!</Button>
+          </div>
        </FullBleedContainer>
     )
- }
+}
