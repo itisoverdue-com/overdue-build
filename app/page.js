ab754c7a370e4d31b4c6af8b7c179a76bb3e2561
Francisco <francisco@fromokaytogreat.com>
2023-07-14 16:44:12 -0700

    :construction: Who Are We section

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import Image from "next/image"
 import Hero from "@/components/Landing/Hero"
 import Card from "@/components/shared/Card"
 import FullBleedContainer from "@/components/Layout/Container/FullBleedContainer"
+import Button from "@/components/shared/Button"
 
 export default function Home() {
    return (
@@ -10,7 +11,44 @@ export default function Home() {
             page="IT IS OVERDUE"
             backgroundImageSrc="https://res.cloudinary.com/di7ejl8jx/image/upload/v1688441392/landing/hero_yntaig.jpg"
          />
-         <FullBleedContainer sx="h-96 bg-blue-200">
+         <FullBleedContainer 
+            sx="bg-background border"
+            childSx="py-28 md:py-32 px-6 lg:px-12"
+         >
+            {/* Who are we - section */}
+            <div className="flex flex-col justify-center items-center text-center md:flex-row md:flex-wrap xl:px-12">
+               {/* Subheader */}
+               <div className="md:w-[55%] md:pr-12 xl:pr-4 lg:w-[45%]">
+                  <h2 className="md:text-left">Who are We?</h2>
+                  <div className="border-t-8 border-primary w-20 rounded-full my-4 md:my-5 md:w-28 mx-auto md:mx-0" />
+                  <h3 className="mx-auto mt-8 px-4 sm:px-8 md:px-0 sm:text-2xl md:text-3xl md:leading-[3rem] font-medium text-left">
+                     OVERDUE is a collective of individuals who pick up trash
+                     together to make their communities cleaner, greener and
+                     friendlier.
+                  </h3>
+                  <p className="mx-auto mt-4 px-4 sm:px-8 md:px-0 md:leading-[2rem] font-normal text-left">
+                     Our philosophy is that if people don&apos;t see litter
+                     around, they will feel less inclined to toss it on the
+                     street.
+                  </p>
+                  {/* add dark variant Button component */}
+                  <Button variant="dark" size="xl" >Learn More</Button>
+               </div>
+               {/* Image Card */}
+               <Card
+                  sx="w-[75vw] aspect-[1/1] mt-8  md:h-[400px] md:w-[45%] md:mt-0 lg:w-[55%] lg:max-w-[560px] lg:mr-0"
+                  variant="image"
+               >
+                  <Image
+                     alt="itisOVERDUE team gathered outside"
+                     src="https://res.cloudinary.com/di7ejl8jx/image/upload/v1688441387/landing/about1_sxanuf.jpg"
+                     fill
+                     style={{
+                        objectFit: "cover",
+                     }}
+                  />
+               </Card>
+            </div>
          </FullBleedContainer>
       </main>
    )
