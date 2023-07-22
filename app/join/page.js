7679f242819d0bd21ceaa7eecae0f806e308a9f7
Jonathan <jonathancarpena99@gmail.com>
2023-07-22 13:49:15 -0700

    :tada: Setup page

--- app/join/page.js
+++ app/join/page.js
@@ -1,3 +1,28 @@
+import PageHero from "@/components/shared/PageHero"
+import FullBleedContainer from "@/components/Layout/Container/FullBleedContainer"
+import Image from "next/image"
+
 export default function Join() {
-   return <div>Join</div>
+   return (
+      <div>
+         <PageHero
+            route="join"
+            header="Join Our Team"
+            backgroundImageSrc="https://res.cloudinary.com/di7ejl8jx/image/upload/v1688441392/backgrounds/volunteer_jo6rbb.jpg"
+         />
+         <FullBleedContainer
+            sx="bg-background"
+            childSx="py-20 lg:py-32 flex flex-col justify-center items-center  text-center"
+         >
+            {/* Title, Accent Line, Subheader */}
+            <>
+               <h2>Be Apart of Overdue</h2>
+               <div className="border-t-8 border-primary w-20 rounded-full my-4 md:my-5 md:w-28" />
+            </>
+
+            {/* Image, Link to Apply Now */}
+            <div className="relative aspect-video"></div>
+         </FullBleedContainer>
+      </div>
+   )
 }
