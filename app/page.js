3aa70321d0fb131b4f3ca1d32ff99cecd10d861e
Francisco <francisco@fromokaytogreat.com>
2023-07-29 15:49:04 -0700

    :fire: remove border style for testing

--- app/page.js
+++ app/page.js
@@ -12,48 +12,49 @@ import WaysToHelp from "@/components/Pages/WaysToHelp"
 export default function Home() {
    return (
       <main>
-         <Hero  
+         <Hero
             page="IT IS OVERDUE"
             backgroundImageSrc="https://res.cloudinary.com/di7ejl8jx/image/upload/v1688441392/landing/hero_yntaig.jpg"
          />
-         <FullBleedContainer 
-            sx="bg-background border"
+         <FullBleedContainer
+            sx="bg-background"
             childSx="py-28 md:py-32 px-6 lg:px-12"
          >
-         {/* Who Are We? section */}
-         <div className="flex flex-col justify-center items-center text-center md:flex-row md:flex-wrap xl:px-12">
-            {/* Who are we - section */}
-            <SectionSubheader
-               header="Who Are We?"
-               sx="md:w-[55%] md:pr-12 lg:w-[45%] xl:pr-4"
-               headerSx="md:text-left"
-            >
-               <h3 className="mx-auto mt-8 px-4 sm:px-8 md:px-0 sm:text-2xl md:text-3xl md:leading-[3rem] font-medium text-left">
-                  OVERDUE is a collective of individuals who pick up trash
-                  together to make their communities cleaner, greener and
-                  friendlier.
-               </h3>
-               <p className="mx-auto mt-4 px-4 sm:px-8 md:px-0 sm:text-l md:text-xlfont-regular text-left">
-                  Our philosophy is that if people don&apos;t see litter
-                  around, they will feel less inclined to toss it on the
-                  street.
-               </p>
+            {/* Who Are We? section */}
+            <div className="flex flex-col justify-center items-center text-center md:flex-row md:flex-wrap xl:px-12">
+               {/* Who are we - section */}
+               <SectionSubheader
+                  header="Who Are We?"
+                  sx="md:w-[55%] md:pr-12 lg:w-[45%] xl:pr-4"
+                  headerSx="md:text-left"
+               >
+                  <h3 className="mx-auto mt-8 px-4 sm:px-8 md:px-0 sm:text-2xl md:text-3xl md:leading-[3rem] font-medium text-left">
+                     OVERDUE is a collective of individuals who pick up trash
+                     together to make their communities cleaner, greener and
+                     friendlier.
+                  </h3>
+                  <p className="mx-auto mt-4 px-4 sm:px-8 md:px-0 sm:text-l md:text-xlfont-regular text-left">
+                     Our philosophy is that if people don&apos;t see litter
+                     around, they will feel less inclined to toss it on the
+                     street.
+                  </p>
                   <div
-                     // when in md do not display the button 
+                     // when in md do not display the button
                      className="hidden md:flex flex-col  justify-start items-start "
                   >
-                  <Button
-                     variant="dark"
-                     href="/about"
-                     size="md"
-                     style={{
-                        marginTop: "14px", width: "fit-content",
-                     }}
-                        >
-                     Know More About Us
-                  </Button>
-               </div>
-            </SectionSubheader>
+                     <Button
+                        variant="dark"
+                        href="/about"
+                        size="md"
+                        style={{
+                           marginTop: "14px",
+                           width: "fit-content",
+                        }}
+                     >
+                        Know More About Us
+                     </Button>
+                  </div>
+               </SectionSubheader>
                <Card
                   sx="hidden md:flex aspect-[1/1] mt-8  md:h-[400px] md:w-[45%] md:mt-0 lg:w-[55%] lg:max-w-[560px] lg:mr-0"
                   variant="image"
@@ -67,7 +68,7 @@ export default function Home() {
                      }}
                   />
                </Card>
-         </div>
+            </div>
             <EffortStats />
             <WhatWeDo variant="home" />
             <WaysToHelp />
@@ -76,7 +77,7 @@ export default function Home() {
             {/* Our Partners section */}
             {/* Become a Sponsor section */}
             {/* Subscribe to Newsletter section */}
-            <MembersCarousel /> 
+            <MembersCarousel />
          </FullBleedContainer>
       </main>
    )
