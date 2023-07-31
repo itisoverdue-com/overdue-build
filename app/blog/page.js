816a2661e6574868f143c90548d9865124588a35
Jonathan <jonathancarpena99@gmail.com>
2023-07-31 12:27:05 -0700

    :tada: Setup page with Containers

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,3 +1,25 @@
-export default function Blog() {
-   return <div>Blog</div>
+import PageHero from "@/components/shared/PageHero"
+import FullBleedContainer from "@/components/Layout/Container/FullBleedContainer"
+
+export default function BlogPage() {
+   return (
+      <div>
+         <PageHero
+            route="blog"
+            header="blog"
+            backgroundImageSrc="https://res.cloudinary.com/di7ejl8jx/image/upload/v1688441386/backgrounds/blog_up1rht.jpg"
+         />
+         <FullBleedContainer
+            sx="bg-background"
+            childSx="py-28 flex flex-col justify-center items-center text-center md:py-32"
+         >
+            {/* Title, Accent Line, Subheader */}
+            <>
+               <h2>IT IS OVERDUE</h2>
+               <div className="border-t-8 border-primary w-20 rounded-full my-4 md:my-5 md:w-28" />
+               <h3>We are a team based in Woodland Hills, CA.</h3>
+            </>
+         </FullBleedContainer>
+      </div>
+   )
 }
