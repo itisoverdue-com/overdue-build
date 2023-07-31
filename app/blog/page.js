a035b03a0572861e7aa0f83e3cbe8d41cc14aa77
Jonathan <jonathancarpena99@gmail.com>
2023-07-31 12:58:03 -0700

    :construction: Fetched data from the Wordpress blog

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,7 +1,38 @@
+"use client"
+import { useState, useEffect } from "react"
 import PageHero from "@/components/shared/PageHero"
 import FullBleedContainer from "@/components/Layout/Container/FullBleedContainer"
 
 export default function BlogPage() {
+   const [blogs, setBlogs] = useState([])
+   const [page, setPage] = useState(0)
+   const [loading, setLoading] = useState(true)
+   const [errors, setErrors] = useState(null)
+
+   useEffect(() => {
+      async function getBlogs() {
+         setLoading(true)
+         try {
+            const res = await fetch(
+               "https://blog.itisoverdue.org/wp-json/wp/v2/posts?per_page=100&order=desc&status=publish"
+            )
+            const data = await res.json()
+            setBlogs(data)
+         } catch (error) {
+            setErrors(error)
+         }
+         setLoading(false)
+      }
+      getBlogs()
+   }, [])
+
+   console.log(blogs)
+
+   // Date
+   // Title
+   // Author
+   // Slug
+   // Image
    return (
       <div>
          <PageHero
@@ -14,11 +45,15 @@ export default function BlogPage() {
             childSx="py-28 flex flex-col justify-center items-center text-center md:py-32"
          >
             {/* Title, Accent Line, Subheader */}
-            <>
-               <h2>IT IS OVERDUE</h2>
-               <div className="border-t-8 border-primary w-20 rounded-full my-4 md:my-5 md:w-28" />
-               <h3>We are a team based in Woodland Hills, CA.</h3>
-            </>
+            <section>
+               {loading ? (
+                  <p>Loading...</p>
+               ) : blogs.length ? (
+                  <ol>BLOGS</ol>
+               ) : (
+                  <p>NO BLOGS</p>
+               )}
+            </section>
          </FullBleedContainer>
       </div>
    )
