ee637faf8f29a24d53b2f05afbc8b07d265bba8f
Jonathan <jonathancarpena99@gmail.com>
2023-08-01 13:04:58 -0700

    :art: Adjusted Container width to be 1400px

--- components/Layout/Container/index.js
+++ components/Layout/Container/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 function Container({ children, sx = "" }) {
-   return <div className={`max-w-[1512px] mx-auto px-3 ${sx}`}>{children}</div>
+   return <div className={`max-w-[1400px] mx-auto px-3 ${sx}`}>{children}</div>
 }
 
 export default Container
