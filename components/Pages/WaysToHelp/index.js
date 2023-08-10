0822a9113ed2697b38405e9810c5f0dadcdc56d3
Tim Kent <timdekent@gmail.com>
2023-08-10 16:06:32 -0700

    update cards layout and styling

--- components/Pages/WaysToHelp/index.js
+++ components/Pages/WaysToHelp/index.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react"
 import Image from "next/image"
 
 import SectionSubheader from "../SectionSubheader"
@@ -5,29 +6,42 @@ import Card from "@/components/shared/Card"
 import Button from "@/components/shared/Button"
 
 export default function WaysToHelp() {
+   // detect screen width
+   const [windowWidth, setWindowWidth] = useState(0)
+   useEffect(() => {
+      const resize = () => setWindowWidth(window.innerWidth)
+      window.addEventListener("resize", resize)
+      resize()
+      return () => window.removeEventListener("resize", resize)
+   }, [])
+
    const cards = [
       {
+         id: 1,
          text: "Sponsor",
          lgText: "Become a Sponsor",
          imgSrc:
             "https://res.cloudinary.com/di7ejl8jx/image/upload/v1688441374/HandshakeIcon_shorny.svg",
-         href: "/sponsor",
+         page: "sponsor",
       },
       {
+         id: 2,
          text: "Donate",
          lgText: "Donate",
          imgSrc:
             "https://res.cloudinary.com/di7ejl8jx/image/upload/v1688441374/DonateIcon_t2xdga.svg",
-         href: "/donate",
+         page: "donate",
       },
       {
+         id: 3,
          text: "Join",
          lgText: "Join Our Team",
          imgSrc:
             "https://res.cloudinary.com/di7ejl8jx/image/upload/v1688441374/ClipboardIcon_ifyl2r.svg",
-         href: "/join",
+         page: "join",
       },
    ]
+
    return (
       <section>
          <div className="flex flex-col justify-center text-center xl:px-12">
@@ -36,35 +50,38 @@ export default function WaysToHelp() {
                sx="mt-24"
                borderSx="md:mx-auto lg:ml-0"
             />
-            <div className="mt-8 flex flex-col items-center justify-evenly gap-y-12 md:flex-row md:flex-wrap md:gap-x-4 xl:flex-nowrap xl:justify-between">
-               {cards.map((card) => {
-                  return (
-                     <Card
-                        key={cards.indexOf(card)}
-                        variant="actionItem"
-                        sx="max-w-[300px] mx-0 xl:max-w-[360px]"
-                     >
-                        <div className="relative aspect-square w-[150px] xl:w-[180px]">
-                           <Image
-                              alt={card.text}
-                              src={card.imgSrc}
-                              fill
-                              style={{ objectFit: "cover" }}
-                           />
-                        </div>
-                        <div className="mt-12 xl:hidden">
-                           <Button variant="primary" size="xl" href={card.href}>
-                              {card.text}
-                           </Button>
-                        </div>
-                        <div className="mt-12 max-xl:hidden">
-                           <Button variant="primary" size="xl" href={card.href}>
-                              {card.lgText}
-                           </Button>
-                        </div>
-                     </Card>
-                  )
-               })}
+            <div className="mt-8 flex flex-col items-center justify-evenly gap-y-10 lg:flex-row lg:gap-x-16 xl:gap-x-28 min-[1400px]:gap-x-40">
+               {cards
+                  .sort((a, b) => a.id - b.id)
+                  .map(({ id, text, lgText, imgSrc, page }) => {
+                     return (
+                        <Card
+                           key={id}
+                           // variant="actionItem"
+                           sx="flex items-center w-full max-w-[560px] bg-white rounded-xl shadow-lg py-8 px-4 lg:flex-col lg:gap-y-20 xl:py-12"
+                        >
+                           <div className="flex w-1/2 items-center justify-center border-none border-blue-500 lg:w-[150px]">
+                              <div className="relative aspect-square w-[65%] drop-shadow-[4px_2px_2px_rgba(50,48,42,.75)] lg:w-full ">
+                                 <Image
+                                    alt={text}
+                                    src={imgSrc}
+                                    fill
+                                    className="object-contain"
+                                 />
+                              </div>
+                           </div>
+                           <div className="mx-auto w-fit border-none border-red-500">
+                              <Button
+                                 variant="primary"
+                                 size={windowWidth < 420 ? "lg" : "xl"}
+                                 href={`/${page}`}
+                              >
+                                 {windowWidth < 1280 ? text : lgText}
+                              </Button>
+                           </div>
+                        </Card>
+                     )
+                  })}
             </div>
          </div>
       </section>
