a0b90d75ab554dfc4732777aa9c199c022d99224
Tim Kent <timdekent@gmail.com>
2023-08-17 14:44:43 -0700

    add adaptive modal styling

--- components/Pages/Donate/DonationModal.js
+++ components/Pages/Donate/DonationModal.js
@@ -3,20 +3,25 @@ import { XCircleIcon } from "@heroicons/react/24/outline"
 export default function DonationModal({ setShowModal }) {
    return (
       <div id="donorbox-modal-container">
-         <div id="modal" className="fixed left-[4%] top-12 z-30 w-[92%]">
+         <div
+            id="modal"
+            className="fixed left-1/2 top-1/4 z-30 w-full max-w-[430px] -translate-x-1/2 -translate-y-[20%]"
+         >
             <div
                className="flex justify-end"
                onClick={() => setShowModal(false)}
             >
-               <XCircleIcon className="h-12 w-12 text-white" />
+               <XCircleIcon className="h-12 w-12 text-white hover:cursor-pointer" />
             </div>
-            <div className="mt-2 bg-white">
+            <div className="mt-2">
                <iframe
                   src="https://donorbox.org/embed/donation-overdue"
                   name="donorbox"
                   allowpaymentrequest="allowpaymentrequest"
                   seamless="seamless"
-                  className="h-[475px] max-h-none w-full min-w-[250px] max-w-[500px] border-2"
+                  frameBorder="0"
+                  scrolling="no"
+                  className="h-[650px] max-h-none w-full min-w-[250px] max-w-[500px] border-none"
                ></iframe>
             </div>
          </div>
