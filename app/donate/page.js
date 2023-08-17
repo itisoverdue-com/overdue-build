2c5db3ccf9e2c72aaf0fd12a1b4e555bf280b535
Tim Kent <timdekent@gmail.com>
2023-08-17 14:17:25 -0700

    add qr codes, modal mobile styling, button sx prop

--- app/donate/page.js
+++ app/donate/page.js
@@ -11,70 +11,136 @@ import DonationModal from "@/components/Pages/Donate/DonationModal"
 export default function Donate() {
    const [showModal, setShowModal] = useState(false)
    return (
-      <div>
-         <PageHero
-            route="donate"
-            header="Donate To Us"
-            backgroundImageSrc="https://res.cloudinary.com/di7ejl8jx/image/upload/v1692290089/backgrounds/donate-backbround_pphohl_7d94c3.png"
-         />
-         <FullBleedContainer sx="bg-background" childSx="py-20 lg:py-32">
-            {showModal && <DonationModal setShowModal={setShowModal} />}
-            <section>
-               <div className="className=flex flex-col items-center justify-center text-center md:flex-row md:flex-wrap md:px-4 min-[1400px]:px-0">
-                  <SectionSubheader header="Protect the environment for everyone">
-                     <h3 className="mx-auto mt-8 text-center font-normal text-dark-grey sm:text-2xl md:text-left md:text-3xl md:leading-[3rem]">
-                        With your help, together we can make a difference in
-                        protecting the environment.
-                     </h3>
-                     <h3 className="mx-auto mt-4 text-center font-normal text-dark-grey sm:text-2xl md:text-left md:text-3xl md:leading-[3rem]">
-                        Join us today!
-                     </h3>
-                  </SectionSubheader>
-                  {/* --- Donation Options --- */}
-                  <div className="mt-16 w-full">
-                     <h4 className="text-3xl md:text-4xl">Donation Options</h4>
-                     <div className="mt-8 flex flex-col gap-y-8">
-                        <div className="w-full">
-                           <Button
-                              variant="primary"
-                              size="xl"
-                              href="https://account.venmo.com/u/itisoverdue"
-                           >
-                              <div className="mx-auto flex items-center justify-center">
-                                 Donate with Venmo
-                                 <Image
-                                    src="https://res.cloudinary.com/di7ejl8jx/image/upload/v1692289493/venmo_v7tv4l.png"
-                                    alt="Venmo logo"
-                                    width={28}
-                                    height={28}
-                                    className="ml-2.5"
-                                 />
-                              </div>
-                           </Button>
+      <>
+         {showModal && <DonationModal setShowModal={setShowModal} />}
+         <div>
+            <PageHero
+               route="donate"
+               header="Donate To Us"
+               backgroundImageSrc="https://res.cloudinary.com/di7ejl8jx/image/upload/v1692290089/backgrounds/donate-backbround_pphohl_7d94c3.png"
+            />
+            <FullBleedContainer sx="bg-background" childSx="py-20 lg:py-32">
+               <section>
+                  <div className="className=flex flex-col items-center justify-center text-center md:flex-row md:flex-wrap md:px-4 min-[1400px]:px-0">
+                     <SectionSubheader
+                        header="Protect the environment for everyone"
+                        borderSx="md:mx-auto"
+                     >
+                        <h3 className="mx-auto mt-8 text-center font-normal text-dark-grey sm:text-2xl md:text-3xl md:leading-[3rem]">
+                           With your help, together we can make a difference in
+                           protecting the environment.
+                        </h3>
+                        <h3 className="mx-auto mt-4 text-center font-normal text-dark-grey sm:text-2xl md:text-3xl md:leading-[3rem]">
+                           Join us today!
+                        </h3>
+                     </SectionSubheader>
+                     {/* --- Donation Options --- */}
+                     <div className="mt-16 w-full">
+                        <h4 className="text-3xl md:text-4xl">
+                           Donation Options
+                        </h4>
+                        <div className="mt-8 flex flex-col gap-y-8 lg:mt-12 lg:flex-row lg:justify-center lg:gap-x-12 lg:gap-y-0">
+                           <div>
+                              <Button
+                                 variant="primary"
+                                 size="xl"
+                                 sx="w-full mx-auto sm:w-3/5 lg:w-[320px]"
+                                 href="https://account.venmo.com/u/itisoverdue"
+                              >
+                                 <div className="mx-auto flex items-center justify-center">
+                                    Donate with Venmo
+                                    <Image
+                                       src="https://res.cloudinary.com/di7ejl8jx/image/upload/v1692289493/venmo_v7tv4l.png"
+                                       alt="Venmo icon"
+                                       width={28}
+                                       height={28}
+                                       className="ml-2.5"
+                                    />
+                                 </div>
+                              </Button>
+                           </div>
+                           <div>
+                              <Button
+                                 variant="primary"
+                                 size="xl"
+                                 sx="w-full mx-auto sm:w-3/5 lg:w-[320px]"
+                                 onClick={() => setShowModal((prev) => !prev)}
+                              >
+                                 <div className="mx-auto flex items-center justify-center">
+                                    Donate with Donorbox
+                                    <Image
+                                       src="https://res.cloudinary.com/di7ejl8jx/image/upload/v1692289493/donorbox_v6jd4j.png"
+                                       alt="Donorbox icon"
+                                       width={28}
+                                       height={28}
+                                       className="ml-2.5"
+                                    />
+                                 </div>
+                              </Button>
+                           </div>
                         </div>
-                        <div className="w-full">
-                           <Button
-                              variant="primary"
-                              size="xl"
-                              onClick={() => setShowModal((prev) => !prev)}
-                           >
-                              <div className="mx-auto flex items-center justify-center">
-                                 Donate with Donorbox
+                     </div>
+                     {/* --- Scan to Donate --- */}
+                     <div className="mt-16 w-full">
+                        <h4 className="text-3xl md:text-4xl">Scan to Donate</h4>
+                        <div className="flex flex-col items-center justify-center lg:mt-4 lg:flex-row lg:gap-x-12">
+                           <div className="mt-8 w-full max-w-[380px] rounded-xl border border-lighter-grey bg-white py-6 lg:h-[320px]">
+                              <div className="mx-auto flex w-fit flex-col items-center justify-center gap-y-2">
                                  <Image
-                                    src="https://res.cloudinary.com/di7ejl8jx/image/upload/v1692289493/donorbox_v6jd4j.png"
-                                    alt="Donorbox logo"
-                                    width={28}
-                                    height={28}
-                                    className="ml-2.5"
+                                    src="https://res.cloudinary.com/di7ejl8jx/image/upload/v1692137264/overdue-venmo-qr_spnqmi.svg"
+                                    alt="Venmo QR"
+                                    width={200}
+                                    height={200}
+                                 />
+                                 <Image
+                                    src="https://res.cloudinary.com/di7ejl8jx/image/upload/v1692302219/Venmo_Logo_bwg5oc.svg"
+                                    alt="Venmo logo"
+                                    width={125}
+                                    height={23}
+                                    className="mt-4"
                                  />
                               </div>
-                           </Button>
+                           </div>
+                           <div className="mt-8 w-full max-w-[380px] rounded-xl border border-lighter-grey bg-white py-6 lg:h-[320px]">
+                              <div className="mx-auto flex w-fit flex-col items-center justify-center gap-y-2">
+                                 <Image
+                                    src="https://res.cloudinary.com/di7ejl8jx/image/upload/v1692137264/overdue-donorbox-qr_wfdtdr.png"
+                                    alt="Donorbox QR"
+                                    width={200}
+                                    height={200}
+                                 />
+                                 <Image
+                                    src="https://res.cloudinary.com/di7ejl8jx/image/upload/v1692302958/donorbox-logo_yqh235.png"
+                                    alt="Donorbox logo"
+                                    width={125}
+                                    height={28}
+                                    className="mt-4"
+                                 />
+                              </div>
+                           </div>
                         </div>
                      </div>
                   </div>
-               </div>
-            </section>
-         </FullBleedContainer>
-      </div>
+               </section>
+               <section>
+                  <div className="mx-auto mt-16 w-full text-center lg:w-3/5">
+                     <h5 className="lg:text-2xl">
+                        Thank You for Being the Change!
+                     </h5>
+                     <p className="mt-6">
+                        Every contribution, big or small, plays a vital role in
+                        shaping a better tomorrow.
+                     </p>
+                     <p className="mt-4">
+                        Your support doesn&apos;t just end with your donation;
+                        it&apos;s the beginning of a partnership that
+                        strengthens neighborhoods, nurtures nature, and unites
+                        us all.
+                     </p>
+                  </div>
+               </section>
+            </FullBleedContainer>
+         </div>
+      </>
    )
 }
--- components/Pages/Donate/DonationModal.js
+++ components/Pages/Donate/DonationModal.js
@@ -1,21 +1,30 @@
+import { XCircleIcon } from "@heroicons/react/24/outline"
+
 export default function DonationModal({ setShowModal }) {
    return (
-      <div>
-         <iframe
-            src="https://donorbox.org/embed/donation-overdue"
-            name="donorbox"
-            allowpaymentrequest="allowpaymentrequest"
-            seamless="seamless"
-            frameborder="0"
-            scrolling="no"
-            height="900px"
-            width="100%"
-            style={{
-               maxWidth: "500px",
-               minWidth: "250px",
-               maxHeight: "none!important",
-            }}
-         ></iframe>
+      <div id="donorbox-modal-container">
+         <div id="modal" className="fixed left-[4%] top-12 z-30 w-[92%]">
+            <div
+               className="flex justify-end"
+               onClick={() => setShowModal(false)}
+            >
+               <XCircleIcon className="h-12 w-12 text-white" />
+            </div>
+            <div className="mt-2 bg-white">
+               <iframe
+                  src="https://donorbox.org/embed/donation-overdue"
+                  name="donorbox"
+                  allowpaymentrequest="allowpaymentrequest"
+                  seamless="seamless"
+                  className="h-[475px] max-h-none w-full min-w-[250px] max-w-[500px] border-2"
+               ></iframe>
+            </div>
+         </div>
+         <div
+            id="backdrop"
+            className="fixed left-0 top-0 z-20 h-screen w-full bg-black/[.85]"
+            onClick={() => setShowModal(false)}
+         />
       </div>
    )
 }
