60a98cc8f1ca3c9a7570eae451a2b1f138762072
Tim Kent <timkentdev@gmail.com>
2023-08-21 22:14:59 -0700

    update error and loading comps

--- components/shared/Error.js
+++ components/shared/Error.js
@@ -2,7 +2,7 @@ import { ExclamationTriangleIcon } from "@heroicons/react/24/outline"
 
 export default function Error() {
    return (
-      <div className=" mx-auto py-36 text-center text-light-grey dark:text-white lg:w-1/2">
+      <div className=" mx-auto py-36 text-center text-light-grey lg:w-1/2">
          <ExclamationTriangleIcon className="mx-auto inline-block w-1/4 md:w-1/6 lg:w-1/4" />
          <h3 className="mt-3 text-2xl md:text-4xl">
             Oops, something went wrong.
