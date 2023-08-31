2047f07bdb15a1ca34c8fd344cf6a85a4389ac9b
Tim Kent <timkentdev@gmail.com>
2023-08-31 15:11:33 -0700

    98 update stats component (#100)
    * add react-intersection-observer package. add inview comp to stat list items
    
    * add google-spreadsheet package
    
    * remove google-spreadsheet pack, add papaparse pack. fetch data from google sheet and display
    
    * update sheet url

--- components/Landing/EffortStats.js
+++ components/Landing/EffortStats.js
@@ -1,12 +1,14 @@
 "use client"
-import React from "react"
+import React, { useState, useEffect } from "react"
 import Image from "next/image"
-import { useState } from "react"
 import { useInterval } from "react-use"
+import { InView } from "react-intersection-observer"
+import Papa from "papaparse"
 
 // StatItem component represents a single statistic item
 const StatItem = ({ stat }) => {
    const [displayedTotal, setDisplayedTotal] = useState(0)
+   const [isVisible, setIsVisible] = useState(false)
 
    // Check whether the stat is not 'Days since we started'
    const isNotDaysStat = stat.desc !== "Days since we started"
@@ -14,57 +16,85 @@ const StatItem = ({ stat }) => {
    const finalTotal = isNotDaysStat
       ? parseFloat(stat.total) * 1000 // Converting 'K' representation back to actual number
       : stat.total // Keep as is for 'Days since we started'
-
    useInterval(() => {
-      setDisplayedTotal((prev) => {
-         if (prev < finalTotal) {
-            return prev + Math.ceil(finalTotal / 100) // Increase by 1% each time
-         }
-         return finalTotal
-      })
+      isVisible &&
+         setDisplayedTotal((prev) => {
+            if (prev < finalTotal) {
+               return prev + Math.ceil(finalTotal / 100) // Increase by 1% each time
+            }
+            return finalTotal
+         })
    }, 20) // Update every 20 milliseconds
 
    return (
-      <li className="flex flex-col items-center justify-center text-center">
-         <div className="flex justify-center">
-            <Image
-               src={stat.image}
-               alt={`Icon for ${stat.desc}`}
-               width={80}
-               height={80}
-               placeholder="blur"
-               blurDataURL="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iODBweCIgaGVpZ2h0PSI4MHB4Ii8+Cg=="
-            />
-         </div>
-         <div className="pt-4 text-4xl font-bold dark:text-white">
-            {isNotDaysStat ? toKFormat(displayedTotal) : displayedTotal}
-         </div>
-         <div className="text-md text-grey dark:text-white">{stat.desc}</div>
-      </li>
+      <InView
+         as="div"
+         threshold={0.75}
+         triggerOnce
+         onChange={(inView) => setIsVisible(inView)}
+      >
+         <li className="flex flex-col items-center justify-center text-center">
+            <div className="flex justify-center">
+               <Image
+                  src={stat.image}
+                  alt={`Icon for ${stat.desc}`}
+                  width={80}
+                  height={80}
+                  placeholder="blur"
+                  blurDataURL="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iODBweCIgaGVpZ2h0PSI4MHB4Ii8+Cg=="
+               />
+            </div>
+            <div className="pt-4 text-4xl font-bold dark:text-white">
+               {isNotDaysStat ? toKFormat(displayedTotal) : displayedTotal}
+            </div>
+            <div className="text-md text-grey dark:text-white">{stat.desc}</div>
+         </li>
+      </InView>
    )
 }
 
 // stats is the array of statistics data to display
 const EffortStats = () => {
+   const [collected, setCollected] = useState(0)
+   const [effort, setEffort] = useState(0)
+   const [volunteers, setVolunteers] = useState(0)
+
+   // fetch data from google sheet
+   useEffect(() => {
+      Papa.parse(
+         "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ78InTixyfD1RLkpfRpr833h9aEdn_YDURZseJxOaYOyO8anJgXmCuVmEMiLjBtuTYI6uQPVhqi9Sc/pub?gid=0&single=true&output=csv",
+         {
+            download: true,
+            header: true,
+            complete: (results) => {
+               const { collected, effort, volunteers } = results.data[0]
+               setCollected(Number(collected))
+               setEffort(Number(effort))
+               setVolunteers(Number(volunteers))
+            },
+         }
+      )
+   }, [])
+
    const stats = [
       {
          image: "https://res.cloudinary.com/di7ejl8jx/image/upload/v1689626737/trash-collected_a4ux4r.svg",
-         total: toKFormat(64000),
+         total: toKFormat(collected),
          desc: "Lbs of Trash Collected",
       },
       {
          image: "https://res.cloudinary.com/di7ejl8jx/image/upload/v1689626737/hours-effort_z0ngip.svg",
-         total: toKFormat(10000),
+         total: toKFormat(effort),
          desc: "Hours of Effort",
       },
       {
          image: "https://res.cloudinary.com/di7ejl8jx/image/upload/v1689626737/volunteers-enrolled_p6x9no.svg",
-         total: toKFormat(5000),
+         total: toKFormat(volunteers),
          desc: "Volunteers Enrolled",
       },
       {
          image: "https://res.cloudinary.com/di7ejl8jx/image/upload/v1689626737/days-start_efmqvt.svg",
-         total: daysSince("10/18/2020"),
+         total: daysSince("10/18/2020", collected),
          desc: "Days since we started",
       },
    ]
@@ -83,17 +113,18 @@ const EffortStats = () => {
 
 // toKFormat function formats a number to K (for thousands) representation
 const toKFormat = (number) => {
-   return number >= 1000 ? `${(number / 1000).toFixed(1)}K` : number
+   return number >= 1000 ? `${(number / 1000).toFixed(2)}K` : number
 }
 
 // daysSince function calculates the number of days between a provided date and today
-const daysSince = (date) => {
+const daysSince = (date, collected) => {
    const start = new Date(date)
    const end = new Date()
    const millisecondsPerDay = 24 * 60 * 60 * 1000
 
    // We divide by milliseconds per day and round down to ignore partial days
-   return Math.floor((end - start) / millisecondsPerDay)
+   // execution is delayed until google sheet prop is loaded
+   return collected ? Math.floor((end - start) / millisecondsPerDay) : 0
 }
 
 export default EffortStats
